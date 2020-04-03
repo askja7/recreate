566440a9e300807a39c991f734a4cc5c414bf83e
askja7 <54781291+askja7@users.noreply.github.com>
2020-04-03 15:34:54 +0200

    fixed-nav-and-burger

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function setUpBurgerNav(){
         nav.classList.toggle('open');
     })
 }
+
+
+/*
+
+function getData() {
+  fetch("http://lorastaneva.com/kea-projects/second-semester/cms/recreate/wp-json/wp/v2/posts")
+   .then(res=>res.json())
+.then(loopThroughPosts)
+
+function loopThroughPosts(posts){
+  posts.forEach(post=>{
+    const template = document.querySelector("template").content;s
+    const copy = template.cloneNode(true);
+    copy.querySelector(".brand").textContent = post.brand.rendered;
+    copy.querySelector(".model").innerHTML=post.model.rendered;
+      copy.querySelector(".price").innerHTML=post.price.rendered;
+      copy.querySelector(".color").innerHTML=post.color.rendered;
+      copy.querySelector(".stock").innerHTML=post.stock.rendered;
+    document.querySelector("main").appendChild(copy)
+  })
+} */
